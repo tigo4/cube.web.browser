df6bd6a5adf3d379d49b52b573e3f63fcadc5808
tigo <tigo>
2013-11-27 00:33:14 +0000

    draft

---
+++ src/main/moz/cube/chrome/content/cube.js
@@ -0,0 +1,7 @@
+var cubeFunc = function(strin) {
+
+	alert(strin);
+  	var cube = document.getElementById("cube");
+	cube.webNavigation.loadURI("http://www.mozilla.org", 0, null, null, null);
+
+}
---
+++ src/main/moz/cube/defaults/preferences/prefs.js
@@ -0,0 +1,7 @@
+pref("toolkit.defaultChromeURI", "chrome://cube/content/cube.xul");
+/* debug */
+pref("browser.dom.window.dump.enabled", true);
+pref("javascript.options.showInConsole", true);
+pref("javascript.options.strict", true);
+pref("nglayout.debug.disable_xul_cache", true);
+pref("nglayout.debug.disable_xul_fastload", true);
